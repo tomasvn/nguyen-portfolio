cd12e13cb3ac8558a1f94404367615479b64e288
Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
2018-08-24 00:25:55 +0200

    Update html file, js file

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Portfolio Page
 
+### Technical Stack
+
+- HTML
+- SCSS (CSS)
+- JS
+- Gulp
+
+### Designs
+
+### Styleguide
+
+### Changelog
+
+## v0.1
+- [x] Restructure HTML
+- [x] Remove initial styling
+- [x] Update JS file for menu
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,12 @@
 const jsBtn = document.querySelector('.jsBtn')
+const jsMenu = document.querySelector('.jsMenu')
 const body = document.body
 
 jsBtn.addEventListener('click', () => {
-  body.classList.toggle('c-side-menu--is-open')
+  body.classList.toggle('menu--is-open')
+  if (body.classList.contains('menu--is-open')) {
+    jsMenu.classList.remove('is-hidden')
+  } else {
+    jsMenu.classList.add('is-hidden')
+  }
 })
