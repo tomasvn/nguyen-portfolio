aa93f33029918b2a0b6d989e75993777b1f57928
Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
2018-08-25 02:20:32 +0200

    Update sidebar

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,11 @@
 const jsBtn = document.querySelector('.jsBtn')
 const jsClose = document.querySelector('.jsClose')
 const jsMenu = document.querySelector('.jsMenu')
-const body = document.body
 
 jsBtn.addEventListener('click', () => {
-  jsBtn.classList.toggle('is-active')
   jsMenu.classList.toggle('is-open')
 })
 
 jsClose.addEventListener('click', () => {
   jsMenu.classList.toggle('is-open')
-  if (jsMenu.classList.contains('is-open')) {
-    body.style.overflow = 'hidden'
-  }
 })
