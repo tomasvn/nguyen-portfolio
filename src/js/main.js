b3fd25bda0faceb176034a10e8b389b41392e536
Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
2018-08-27 00:37:25 +0200

    Update hp-section, menu, js

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,14 @@
-const jsBtn = document.querySelector('.jsBtn')
-const jsClose = document.querySelector('.jsClose')
+const jsBtn = document.querySelectorAll('.jsBtn')
 const jsMenu = document.querySelector('.jsMenu')
+const body = document.body
 
-jsBtn.addEventListener('click', () => {
-  jsMenu.classList.toggle('is-open')
-})
-
-jsClose.addEventListener('click', () => {
-  jsMenu.classList.toggle('is-open')
+jsBtn.forEach((el) => {
+  el.addEventListener('click', () => {
+    jsMenu.classList.toggle('is-open')
+    if (jsMenu.classList.contains('is-open')) {
+      body.style.overflow = 'hidden'
+    } else {
+      body.style.overflow = 'visible'
+    }
+  })
 })
